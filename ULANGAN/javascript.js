4b0359f60e6eec80701db3752fd3c21c01fe54b0
mawaarya <140318384+mawaarya@users.noreply.github.com>
2023-09-29 09:52:20 +0700

    hari jumat tanggal 29,9,2023
    tugas dii lab javascript

---
+++ ULANGAN/javascript.js
@@ -0,0 +1,44 @@
+//alert ("javascript")
+
+//btn.onclick = function() {
+//const suhu = document.querySelector("#suhu");
+//const konversi = documenn.querySelector("#hasil");
+//let hasil = (9 / 5 * suhu.value) + 32;
+//konversi.innerHtml = hasil;
+//}
+
+//btn.onclick=function(){
+//const meter=document.querySelector("#meter")
+//const hasil=document.querySelector("#hasil")
+//  hasil.innerHtml=meter.value/1000;
+//}
+
+// btn.onclick=function(){
+//     const meter=document.querySelector("#meter")
+//     const hasil=document.querySelector("#hasil")
+//     hasil.innerHTML=meter.value*meter.value;
+// }
+
+// btn.onclick= function() {
+//     document.querySelector("h1").style.backgroundColor = "red";
+// }
+
+// btn.onclick = function() {
+//     document.querySelector("h2").style.fontSize = "20px";
+// }
+
+// btn.onclick=function(){
+//     const div=document.querySelector("div");
+//     const para=document.querySelector("p");
+
+//     para.document.textContent=("belajar javascript");
+//     div.appendChild(para);
+// }
+
+// btn.onclick = function() {
+//     const div = document.querySelector("div");
+//     const h = document.querySelector("h1")
+
+//     h.textContent = "belajar javascript";
+//     div.appendChild(h);
+// }
